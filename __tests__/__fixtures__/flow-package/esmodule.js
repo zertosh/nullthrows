2593ba418928e93f152d42d2acd3e0b8517543d1
Andres Suarez <zertosh@gmail.com>
2018-09-09 03:11:02 +0000

    Clearer flow type tests with commonjs variant

---
+++ __tests__/__fixtures__/flow-package/esmodule.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import nullthrows from '../../..';
+
+var obj = ({}: ?Object);
+
+var bad1: Object = obj;
+var ok1: Object = nullthrows(obj);
+
+nullthrows(null);
+nullthrows(null, 'my error message');
